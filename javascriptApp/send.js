cd226301c742f2306fddfba4fe5456af2b5b8d3c
Daniela0106 <daniela.barrientos.grisales@gmail.com>
2016-05-25 18:10:52 -0500

    adding a test project with nodejs with libraries

---
+++ javascriptApp/send.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+/*
+Daniela Barrientos Grisales
+Desarrollo de Software Basado en Componentes
+*/
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'TestDBG';
+    var msg = 'Hello World!';
+
+    ch.assertQueue(q, {durable: false});
+    ch.sendToQueue(q, new Buffer(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
